0a23bd582c6310d8631b47777ebd66b4bc9cecd8
Destr <2388060740@qq.com>
2019-09-22 00:02:15 +0800

    add a new feature:read

--- Main.java
+++ Main.java
@@ -1,6 +1,23 @@
 package com.address;
+import java.io.*;
 public class Main {
+    public String readFile() throws IOException{
+        String inputPath="D:\\IDEA2019\\IdeaProject\\addressbook\\input.txt";
+        File file=new File(inputPath);
+        FileReader reader =new FileReader(file);
+        char[] ch=new char[10000];
+        reader.read(ch);
+        String st="";
+        for(char c:ch){
+            st+=c;
+        }
+        reader.close();
+        return st;
+    }
+
     public static void main(String[] args) throws IOException{
-		
+        Main fileRW=new Main();
+        String ss=fileRW.readFile();
+        System.out.println(ss);
     }
 }
