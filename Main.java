87808638b784dbeb305d556148b15a9e834cfa30
Destr <2388060740@qq.com>
2019-09-23 19:50:10 +0800

    add a new feature:find the level and name

--- Main.java
+++ Main.java
@@ -5,6 +5,10 @@ import java.util.regex.Pattern;
 
 public class Main {
     private String phoneNumber="";
+    private String name;
+    public void setName(String _name){
+        name=_name;
+    }
     public void setPhoneNumber(String s0)
     {
         phoneNumber=s0;
@@ -36,8 +40,19 @@ public class Main {
             {
                 ans.setPhoneNumber(matcher.group());
             }//寻找手机号并提取出来
-            System.out.println(ans.phoneNumber);
-
+            //System.out.println(ans.phoneNumber);
+            String[] s1=s.split("\\d{11}");
+            s=s1[0]+s1[1];
+            String[] s2=s.split(",");
+            char level=s.charAt(0);//获取难度等级
+            //System.out.println(level);
+            String _name="";
+            for(int i=2;i<s2[0].length();i++){
+                _name+=s2[0].charAt(i);
+            }//提取姓名
+            ans.setName(_name);
+            System.out.println(ans.name);
+            s=s2[1];
         }
     }
 }
