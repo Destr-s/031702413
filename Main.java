8f628fd7c8b814783e4be419857f054d52446375
Destr <2388060740@qq.com>
2019-09-28 15:02:37 +0800

    update

--- Main.java
+++ Main.java
@@ -1,4 +1,3 @@
-package address;
 import java.io.*;
 import java.io.IOException;
 import com.google.gson.*;
@@ -13,22 +12,22 @@ public class Main {
     private String name="";
     private String s="";
     private String[] address=new String[10];
-    public static final String myFirstAddress=
+    private static final String myFirstAddress=
             "北京|天津|河北|山西|辽宁|吉林|黑龙江|上海|江苏|浙江|安徽|福建|江西|山东|河南|湖北|湖南|广东|海南|重庆|四川|贵州|云南|陕西|甘肃|青海";
-    public static final String mysecondAdress=
+    private static final String mysecondAdress=
             "阿坝|阿拉善|阿里|安康|安庆|鞍山|安顺|安阳|澳门|北京|白银|保定|宝鸡|保山|包头|巴中|北海|蚌埠|本溪|毕节|滨州|百色|亳州|重庆|成都|长沙|长春|沧州|常德|昌都|长治|常州|巢湖|潮州|承德|郴州|赤峰|池州|崇左|楚雄|滁州|朝阳|大连|东莞|大理|丹东|大庆|大同|大兴安岭|德宏|德阳|德州|定西|迪庆|东营|鄂尔多斯|恩施|鄂州|福州|防城港|佛山|抚顺|抚州|阜新|阜阳|广州|桂林|贵阳|甘南|赣州|甘孜|广安|广元|贵港|果洛|杭州|哈尔滨|合肥|海口|呼和浩特|海北|海东|海南|海西|邯郸|汉中|鹤壁|河池|鹤岗|黑河|衡水|衡阳|河源|贺州|红河|淮安|淮北|怀化|淮南|黄冈|黄南|黄山|黄石|惠州|葫芦岛|呼伦贝尔|湖州|菏泽|济南|佳木斯|吉安|江门|焦作|嘉兴|嘉峪关|揭阳|吉林|金昌|晋城|景德镇|荆门|荆州|金华|济宁|晋中|锦州|九江|酒泉|昆明|开封|兰州|拉萨|来宾|莱芜|廊坊|乐山|凉山|连云港|聊城|辽阳|辽源|丽江|临沧|临汾|临夏|临沂|林芝|丽水|六安|六盘水|柳州|陇南|龙岩|娄底|漯河|洛阳|泸州|吕梁|马鞍山|茂名|眉山|梅州|绵阳|牡丹江|南京|南昌|南宁|宁波|南充|南平|南通|南阳|那曲|内江|宁德|怒江|盘锦|攀枝花|平顶山|平凉|萍乡|莆田|濮阳|青岛|黔东南|黔南|黔西南|庆阳|清远|秦皇岛|钦州|齐齐哈尔|泉州|曲靖|衢州|日喀则|日照|上海|深圳|苏州|沈阳|石家庄|三门峡|三明|三亚|商洛|商丘|上饶|山南|汕头|汕尾|韶关|绍兴|邵阳|十堰|朔州|四平|绥化|遂宁|随州|宿迁|宿州|天津|太原|泰安|泰州|台州|唐山|天水|铁岭|铜川|通化|通辽|铜陵|铜仁|台湾|武汉|乌鲁木齐|无锡|威海|潍坊|文山|温州|乌海|芜湖|乌兰察布|武威|梧州|厦门|西安|西宁|襄樊|湘潭|湘西|咸宁|咸阳|孝感|邢台|新乡|信阳|新余|忻州|西双版纳|宣城|许昌|徐州|香港|锡林郭勒|兴安|银川|雅安|延安|延边|盐城|阳江|阳泉|扬州|烟台|宜宾|宜昌|宜春|营口|益阳|永州|岳阳|榆林|运城|云浮|玉树|玉溪|玉林|枣庄|扎赉特旗|扎鲁特旗|张家港|张家界|张家口|张掖|漳州|湛江|肇庆|昭通|郑州|镇海区|镇江|中山|中卫|舟山|珠海|驻马店|准格尔旗|卓尼|株洲|淄博|自贡|资阳";
-    public String myThirdAddress="区|县|市";
-    public String myForthAddress="街道|镇|乡";
-    public String myFifthAddress="路|街|巷";
-    public String mySixthAddress="号";
-    public  void setName(String _name){
+    private String myThirdAddress="区|县|市";
+    private String myForthAddress="街道|镇|乡";
+    private String myFifthAddress="路|街|巷";
+    private String mySixthAddress="号";
+    private  void setName(String _name){
         name=_name;
     }
-    public  void setPhoneNumber(String s0)
+    private  void setPhoneNumber(String s0)
     {
         phoneNumber=s0;
     }
-    public void init(){
+    private void init(){
         phoneNumber="";
         name="";
         s="";
@@ -38,20 +37,20 @@ public class Main {
 
     public void writeFile(JsonArray arr,String outPutPath) throws IOException{
         File file=new File(outPutPath);
-        if(!file.exists())
-            file.createNewFile();
+        //if(!file.exists())
+        //    file.createNewFile();
         BufferedWriter writer=
                 new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file),StandardCharsets.UTF_8));
-        String ansjon=arr.toString();
-        JsonParser jsonParser=new JsonParser();
-        JsonArray jsonArray=jsonParser.parse(ansjon).getAsJsonArray();
-        Gson gson=new GsonBuilder().setPrettyPrinting().create();
-        writer.write(gson.toJson(jsonArray));
+        //String ansjon=arr.toString();
+        //JsonParser jsonParser=new JsonParser();
+        //JsonArray jsonArray=jsonParser.parse(ansjon).getAsJsonArray();
+        //Gson gson=new GsonBuilder().setPrettyPrinting().create();
+        writer.write(arr.toString());
         writer.flush();
         writer.close();
     }//向文件中写入结果
 
-    public  Main getFirstAddress(Main res){
+    private  Main getFirstAddress(Main res){
         String first="";
         Pattern pattern1=Pattern.compile(myFirstAddress);
         Matcher matcher1=pattern1.matcher(res.s);
@@ -108,7 +107,7 @@ public class Main {
         return res;
     }//提取一级地址
 
-    public  Main getSecondAddress(Main res){
+    private  Main getSecondAddress(Main res){
         if(res.address[1].equals("上海")||res.address[1].equals("北京")||res.address[1].equals("天津"))
         {
             res.address[2]=res.address[1]+'市';
@@ -131,7 +130,7 @@ public class Main {
         return res;
     }//提取二级地址
 
-    public Main getThirdAddress(Main res){
+    private Main getThirdAddress(Main res){
 
         String third="";
         Pattern pattern=Pattern.compile(myThirdAddress);
@@ -155,7 +154,7 @@ public class Main {
         return res;
     }//提取三级地址
 
-    public Main getForthAddress(Main res){
+    private Main getForthAddress(Main res){
         String forth="";
         Pattern pattern=Pattern.compile(myForthAddress);
         Matcher matcher=pattern.matcher(res.s);
@@ -169,7 +168,7 @@ public class Main {
         return res;
     }//提取四级地址
 
-    public Main getFifthAddress(Main res){
+    private Main getFifthAddress(Main res){
         String fifth="";
         Pattern pattern=Pattern.compile(myFifthAddress);
         Matcher matcher=pattern.matcher(res.s);
@@ -184,7 +183,7 @@ public class Main {
         return res;
     }//提取五级地址
 
-    public Main getSixthAddress(Main res){
+    private Main getSixthAddress(Main res){
         String sixth="";
         Pattern pattern=Pattern.compile(mySixthAddress);
         Matcher matcher=pattern.matcher(res.s);
